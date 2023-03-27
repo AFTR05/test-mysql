c7791008a21a86b4d45f5853431aea6aeeb31275
Andres <atoro1025@cue.edu.co>
2023-03-27 18:38:10 -0500

    Finish request

---
+++ src/main/java/org/example/ConnectionBD.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionBD {
+    private static String url="jdbc:mysql://localhost:3306/test";
+    private static String user="root";
+    private static String password="andreS0503";
+    private static Connection connection;
+    public static Connection getInstance() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(url,user,password);
+        }
+        return connection;
+    }
+}
+
