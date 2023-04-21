db45847cca7a0b979b8a2a66ec0e18b77ffb7140
Andres <atoro1025@cue.edu.co>
2023-04-21 15:29:09 -0500

    Descomentar

--- src/main/java/org/example/application/MainProductoCategoria.java
+++ src/main/java/org/example/application/MainProductoCategoria.java
@@ -15,18 +15,16 @@ public class MainProductoCategoria {
     public static void main(String[] args) {
         try (Connection conn = ConnectionBD.getInstance()){
             ProductCategoryServiceImpl productService=new ProductCategoryServiceImpl();
-/*            productService.getlist();
-            productService.getbyId(1L);*/
+            productService.getlist();
+            productService.getbyId(1L);
             productService.delete(18L);
             productService.delete(17L);
             productService.delete(16L);
-
-
             productService.getlist();
-/*            productService.save(new ProductCategory("Keke",5000.0, LocalDate.of(2023,03,27),new Category(1,"food")));
+            productService.save(new ProductCategory("Keke",5000.0, LocalDate.of(2023,03,27),new Category(1,"food")));
             productService.getlist();
             productService.update(1L,new ProductCategory("Max Steel",50000.0, LocalDate.of(2023,03,27),new Category(2,"toy")));
-            productService.getlist();*/
+            productService.getlist();
         }catch (SQLException e) {
             e.printStackTrace();
         }
